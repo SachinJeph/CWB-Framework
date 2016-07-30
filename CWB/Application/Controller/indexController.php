54d104278fa07b8df6597ac19dd4c7fdbfaae121
Nitin Goyal <nitinpiet125@poornima.org>
2016-07-30 18:09:48 +0530

    Update indexController.php

--- CWB/Application/Controller/indexController.php
+++ CWB/Application/Controller/indexController.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace CWB\Application\Controller;
+
+/**
+* Class indexController
+* Get All the Information About the System
+*
+* @package CWB
+**/
+class IndexController {
+	public function index(){
+		echo "Hello";
+	}
+}
