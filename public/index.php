812378caa6e7f18ece7a9951da3ad7c4eddd7c60
Nitin Goyal <nitinpiet125@poornima.org>
2016-07-30 15:12:02 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,21 @@
 <?php
+/**
+* CWB - A PHP Framework For Faster Develpoment
+* @package CWB
+* @author Nitin Goyal <nitinpiet125@poornima.org>
+**/
+
+define('CWB_START', microtime(true));
+
+// define the directory separator
+define('DS', DIRECTORY_SEPARATOR);
+
+// define the application path
+define('ROOT', dirname(dirname(__FILE__)));
+
+define('CWB_DIR', ROOT . DS . 'CWB' . DS);
+define('PUBLIC_DIR', __DIR__ . DS);
+
+require CWB_DIR . 'Core' . DS . 'Autoloader.php';
+
+CWB\Core\Start::Main();
